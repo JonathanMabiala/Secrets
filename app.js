b2e25d853d1ceb2410ccdcbe94d6807bfa094611
JonathanMabiala <62263731+JonathanMabiala@users.noreply.github.com>
2023-10-12 08:18:49 -0400

    initial code

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 //jshint esversion:6
+import express from "express";
+import ejs from "ejs";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
